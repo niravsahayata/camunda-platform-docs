61ace82eb0467887d0c4559760e681fec6fa228e
Igor Petrov <igor.petrov-ext@camunda.com>
2023-03-21 18:29:07 +0200

    docs(connectors): added docs for GitLab connector

--- sidebars.js
+++ sidebars.js
@@ -237,6 +237,7 @@ module.exports = {
             "components/connectors/out-of-the-box-connectors/aws-sqs",
             "components/connectors/out-of-the-box-connectors/aws-lambda",
             "components/connectors/out-of-the-box-connectors/github-webhook",
+            "components/connectors/out-of-the-box-connectors/gitlab",
             "components/connectors/out-of-the-box-connectors/googledrive",
             "components/connectors/out-of-the-box-connectors/graphql",
             "components/connectors/out-of-the-box-connectors/http-webhook",
